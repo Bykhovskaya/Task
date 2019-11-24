281263f5818b11370c0f1808f985f38f0861bfc2
Bykhovskaya <57328662+Bykhovskaya@users.noreply.github.com>
2019-11-24 17:19:36 +0300

    Update exception.py

--- exception.py
+++ exception.py
@@ -3,33 +3,30 @@
 user_input = input('Введите знак вычисления (+, -, *, /) и два целых положительных числа через пробел')
 input_list = user_input.split(' ')
 
-assert input_list[0] in '+-*/', 'Введен некорректный оператор. Повторите ввод'
+assert input_list[0] in ('+', '-', '*', '/'), 'Введен некорректный оператор. Повторите ввод'
 
-for elem in input_list:
-    if elem[0] in '+-*/':
-        try:
-            a = int(input_list[1])
-            b = int(input_list[2])
-            if int(input_list[1]) < 0 or int(input_list[2]) < 0:
-                print('Вы ввели отрицательное число')
-                break
-            if input_list[0] == '+':
-                result = (a+b)
+try:
+        a = int(input_list[1])
+        b = int(input_list[2])
+        if a < 0 or b < 0:
+            print('Вы ввели отрицательное число')
+        if input_list[0] == '+' and a > 0 and b > 0:
+            result = a+b
             #print(result)
-            if input_list[0] == '-':
-                result = (a-b)
-            if input_list[0] == '*':
-                result = (a*b)
-            if input_list[0] == '/':
-                result = (a/b)
-            print(result)
-        except ValueError:
-            print('Некорректный ввод данных')
-        except ZeroDivisionError:
-            print('На ноль делить нельзя')
-
-        except IndexError:
-            print('Некорректный ввод данных')
-        except Exception as e:
-            print('Исключение', e, 'тип:', type(e))
-        break
+        if input_list[0] == '-' and a > 0 and b > 0:
+            result = a-b
+        if input_list[0] == '*' and a > 0 and b > 0:
+            result = a*b
+        if input_list[0] == '/' and a > 0 and b > 0:
+            result = a/b
+        print(result)
+except NameError:
+        print('Некорректный ввод данных')
+except ValueError:
+        print('Некорректный ввод данных')
+except ZeroDivisionError:
+        print('На ноль делить нельзя')
+except IndexError:
+        print('Некорректный ввод данных')
+except Exception as e:
+        print('Исключение', e, 'тип:', type(e))
