3512368e92364ead5b4ddd62472730cae6d0763c
Bykhovskaya <57328662+Bykhovskaya@users.noreply.github.com>
2019-12-07 16:51:59 +0300

    Update Open_read_file.py

--- Open_read_file.py
+++ Open_read_file.py
@@ -21,29 +21,7 @@ def get_cook_book():
 #get_cook_book()
 
 def shop_list(dishes, person_count):
-    shop_list = {}
-    shop_list_1 = {}
-    cook_book = get_cook_book()
-    dishes_list = list(dishes)
-    
-    for dishes_list[0] in dishes_list:
-        for ingredients in cook_book[dishes[0]]:
-            menu = dict(ingredients)
-            menu['quantity'] *= int(person_count)
-            if menu['ingridient_name'] not in shop_list:
-                shop_list[menu['ingridient_name']] = {'measure': menu['measure'], 'quantity': menu['quantity']}
-
-    for dishes_list[1] in dishes_list:
-        for ingredients_1 in cook_book[dishes[1]]:
-                menu_1 = dict(ingredients_1)
-                if dishes_list[1] == dishes_list[0]:
-                    menu_1['quantity'] *= (int(person_count)*2)
-                elif dishes_list[1] != dishes_list[0]:
-                    menu_1['quantity'] *= int(person_count)
-                if menu_1['ingridient_name'] not in shop_list_1:
-                       shop_list_1[menu_1['ingridient_name']] = {'measure': menu_1['measure'], 'quantity': menu_1['quantity']}
-
-    shop_list_by_dises = {**shop_list, **shop_list_1}
-    print(shop_list_by_dises)        
+   
+  
    
 shop_list(['Омлет', 'Омлет'], 3)
